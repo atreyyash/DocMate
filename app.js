d3f0e5bc917137a22fdaf85c632e916a4774e9d9
triptisingla <triptisingla306@gmail.com>
2023-06-01 23:25:23 +0530

    signup- login

--- app.js
+++ app.js
@@ -4,12 +4,52 @@ const app = express();
 const PORT = 4444;
 const hbs = require('hbs');
 const bodyParser = require('body-parser');
+const mongoose  = require('mongoose');
+
+const dotenv = require('dotenv');
+const { unsubscribe } = require('./routes/users');
+dotenv.config();
+
+const url=process.env.MONGO_URL;
+
 hbs.registerPartials(__dirname + '/views/partials');
 
 app.set('view engine', 'hbs');
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(PORT,()=>{
-    console.log(`http://localhost:`+PORT);
+const users=require('./routes/users')
+
+app.use('/users',users)
+
+
+
+
+
+mongoose.connect(url)
+// console.log(url);
+mongoose.connection.on('connected',()=>{
+    console.log("mongoose is connected")
+    app.listen(PORT,()=>{
+        console.log(`http://localhost:`+PORT);
+    })
+})
+
+mongoose.connection.on('disconnected',()=>{
+    console.log("mongoose is disconnected")
+    process.exit(1);
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log(err);
+    process.exit(1);
+})
+
+
+process.on('SIGINT', () => {
+    console.log("App is terminating");
+    mongoose.connection.close(() => {
+        console.log("Mongoose default connection closed");
+        process.exit(0);
+    })
 })
